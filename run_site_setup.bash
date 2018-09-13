0c5b9b162ecf7416af1db86c1ba37533374d9a0c
David Lougheed <david.lougheed@gmail.com>
2018-09-13 19:30:31 -0400

    Take advantage of executable manage.py in run_site_setup script.

--- run_site_setup.bash
+++ run_site_setup.bash
@@ -7,6 +7,6 @@ rm -rf ./site_env 2> /dev/null
 virtualenv -p python3 ./site_env
 PS1="" source site_env/bin/activate
 pip install -r ./requirements.txt
-python ./manage.py makemigrations
-python ./manage.py migrate
+./manage.py makemigrations
+./manage.py migrate
 deactivate
