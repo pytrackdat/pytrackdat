5abee3fb59903cc1a0326c49ebfce50e921f4c68
David Lougheed <david.lougheed@gmail.com>
2018-11-01 22:29:10 -0400

    Fix some issues with the django setup scripts and virtual environments.

--- create_django_site.bash
+++ create_django_site.bash
@@ -6,9 +6,9 @@ cd $2
 rm -rf ./tmp_env 2> /dev/null
 rm -rf ./$1 2> /dev/null
 virtualenv -p python3 ./tmp_env
-PS1="" source tmp_env/bin/activate
+PS1="" source ./tmp_env/bin/activate
 pip install -r ../util_files/requirements_setup.txt
-django-admin startproject $1
+python ./tmp_env/bin/django-admin startproject $1
 cp ../util_files/requirements.txt $1/
 cd $1
 chmod +x ./manage.py
--- run_site_setup.bash
+++ run_site_setup.bash
@@ -5,7 +5,7 @@ set -eu
 cd $2/$1
 rm -rf ./site_env 2> /dev/null
 virtualenv -p python3 ./site_env
-PS1="" source site_env/bin/activate
+PS1="" source ./site_env/bin/activate
 pip install -r ./requirements.txt
 ./manage.py makemigrations
 ./manage.py migrate
