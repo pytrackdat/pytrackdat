af21812431b8783ac4562e3871b1e689b4128ce0
David Lougheed <david.lougheed@gmail.com>
2018-11-02 13:50:26 -0400

    Begin work on dockerizing end result for easy deployment.

--- create_django_site.bash
+++ create_django_site.bash
@@ -10,7 +10,11 @@ PS1="" source ./tmp_env/bin/activate
 pip install -r ../util_files/requirements_setup.txt
 python ./tmp_env/bin/django-admin startproject $1
 cp ../util_files/requirements.txt $1/
+cp ../util_files/Dockerfile $1/
+cp ../util_files/docker-compose.yml $1/
+cp ../util_files/nginx.conf $1/
 cd $1
+sed -i '' -e "s/SITE_NAME/$1/g" ./Dockerfile
 chmod +x ./manage.py
 ./manage.py startapp core
 cp -r ../../app_includes/* ./core/
