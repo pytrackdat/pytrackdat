1df92a103e1406572cb681da860df505dada80f0
David Lougheed <david.lougheed@gmail.com>
2018-12-01 20:15:46 -0500

    Add work-in-progress windows site creation script

---
+++ create_django_site.bat
@@ -0,0 +1,18 @@
+@echo off
+
+cd %2
+rmdir /Q /S .\tmp_env
+rmdir /Q /S .\$1
+virtualenv -p python3 .\tmp_env
+call .\tmp_env\Scripts\activate.bat
+pip install -r ..\util_files\requirements_setup.txt
+python .\tmp_env\Scripts\django-admin startproject %1
+copy /B ..\util_files\requirements.txt %1\
+copy /B ..\util_files\Dockerfile %1\
+copy /B ..\util_files\docker-compose.yml %1\
+copy /B ..\util_files\nginx.conf %1\
+cd %1
+powershell -Command "(gc Dockerfile) -replace 'SITE_NAME', '%1' | Out-File Dockerfile"
+python ./manage.py startapp core
+xcopy ..\..\app_includes\ %1\
+copy /B ..\..\common.py\ %1\
