d45c3d991fdbcd29c13ef3e99f10be8866620aab
David Lougheed <david.lougheed@gmail.com>
2018-12-01 22:10:48 -0500

    Further tweaks to create_django_site.bat

--- create_django_site.bat
+++ create_django_site.bat
@@ -9,14 +9,14 @@ if errorlevel 1 (
 )
 call tmp_env\Scripts\activate.bat
 pip install -r ..\util_files\requirements_setup.txt
-python .\tmp_env\Scripts\django-admin startproject %1
-copy /B ..\util_files\requirements.txt %1\
-copy /B ..\util_files\Dockerfile %1\
-copy /B ..\util_files\docker-compose.yml %1\
-copy /B ..\util_files\nginx.conf %1\
-cd %1
+python .\tmp_env\Scripts\django-admin startproject "%1"
+copy /B ..\util_files\requirements.txt "%1\"
+copy /B ..\util_files\Dockerfile "%1\"
+copy /B ..\util_files\docker-compose.yml "%1\"
+copy /B ..\util_files\nginx.conf "%1\"
+cd "%1"
 powershell -Command "(gc Dockerfile) -replace 'SITE_NAME', '%1' | Out-File Dockerfile"
-python ./manage.py startapp core
-xcopy ..\..\app_includes\ %1\
-copy /B ..\..\common.py\ %1\
+python manage.py startapp core
+xcopy ..\..\app_includes "%1"
+copy /B ..\..\common.py "%1"
 deactivate
