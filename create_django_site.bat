fd586273d1c1e77c31195ee3af9f6e4f38665c40
David Lougheed <david.lougheed@gmail.com>
2018-12-01 22:02:10 -0500

    Tweak create site batch file

--- create_django_site.bat
+++ create_django_site.bat
@@ -1,10 +1,13 @@
 @echo off
 
-cd %2
-rmdir /Q /S .\tmp_env
-rmdir /Q /S .\$1
-virtualenv -p python3 .\tmp_env
-call .\tmp_env\Scripts\activate.bat
+cd "%2"
+rmdir /Q /S tmp_env > nul 2> nul
+rmdir /Q /S "%1" > nul 2> nul
+virtualenv -p python3 tmp_env > nul 2> nul
+if errorlevel 1 (
+    virtualenv -p python tmp_env
+)
+call tmp_env\Scripts\activate.bat
 pip install -r ..\util_files\requirements_setup.txt
 python .\tmp_env\Scripts\django-admin startproject %1
 copy /B ..\util_files\requirements.txt %1\
@@ -16,3 +19,4 @@ powershell -Command "(gc Dockerfile) -replace 'SITE_NAME', '%1' | Out-File Docke
 python ./manage.py startapp core
 xcopy ..\..\app_includes\ %1\
 copy /B ..\..\common.py\ %1\
+deactivate
