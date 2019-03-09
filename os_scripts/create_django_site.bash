4405a2c82d6b9f888aebd1f6bcdc6fadf779504e
David Lougheed <david.lougheed@gmail.com>
2019-03-09 11:35:26 -0500

    Add comments to create_django_site scripts

--- os_scripts/create_django_site.bash
+++ os_scripts/create_django_site.bash
@@ -2,23 +2,44 @@
 
 set -eu
 
+# Enter the temporary site construction directory
 cd $2
+
+# Remove existing site data if it exists
 rm -rf ./tmp_env 2> /dev/null
 rm -rf ./$1 2> /dev/null
+
+# Create and activate the virtual environment used for setup
 virtualenv -p python3 ./tmp_env
 PS1="" source ./tmp_env/bin/activate
+
+# Install the dependencies required for setup
 pip install -r ../util_files/requirements_setup.txt
+
+# Start the Django site
 python ./tmp_env/bin/django-admin startproject $1
+
+# Copy pre-built files to the site folder
 cp ../util_files/requirements.txt $1/
 cp ../util_files/Dockerfile $1/
 cp ../util_files/docker-compose.yml $1/
 cp ../util_files/nginx.conf $1/
 cp ../util_files/export_labels.R $1/
 cp ../util_files/install_dependencies.R $1/
+
+# Enter the Django site directory
 cd $1
 sed -i '' -e "s/SITE_NAME/$1/g" ./Dockerfile
+
+# Make Django site manager script executable
 chmod +x ./manage.py
+
+# Create the Django application for the models
 ./manage.py startapp core
+
+# Copy pre-built application scripts to the application
 cp -r ../../app_includes/* ./core/
 cp ../../common.py ./core/
+
+# Deactivate the temporary setup virtual environment
 deactivate
--- os_scripts/create_django_site.bat
+++ os_scripts/create_django_site.bat
@@ -1,24 +1,43 @@
 @echo off
 
+rem Enter the temporary site construction directory
 cd "%2"
+
+rem Remove existing site data if it exists
 rmdir /Q /S tmp_env > nul 2> nul
 rmdir /Q /S "%1" > nul 2> nul
+
+rem Create and activate the virtual environment used for setup
 virtualenv -p python3 tmp_env > nul 2> nul
 if errorlevel 1 (
     virtualenv -p python tmp_env
 )
 call tmp_env\Scripts\activate.bat
+
+rem Install the dependencies required for setup
 pip install -r ..\util_files\requirements_setup.txt
+
+rem Start the Django site
 python .\tmp_env\Scripts\django-admin startproject "%1"
+
+rem Copy pre-built files to the site folder
 copy /B ..\util_files\requirements.txt "%1\"
 copy /B ..\util_files\Dockerfile "%1\"
 copy /B ..\util_files\docker-compose.yml "%1\"
 copy /B ..\util_files\nginx.conf "%1\"
 copy /B ..\util_files\export_labels.R "%1\"
 copy /B ..\util_files\install_dependencies.R "%1\"
+
+rem Enter the Django site directory
 cd "%1"
 powershell -Command "(gc Dockerfile) -replace 'SITE_NAME', '%1' | Out-File Dockerfile"
+
+rem Create the Django application for the models
 python manage.py startapp core
+
+rem Copy pre-built application scripts to the application
 xcopy ..\..\app_includes core /s /e
 copy /B ..\..\common.py core
+
+rem Deactivate the temporary setup virtual environment
 deactivate
