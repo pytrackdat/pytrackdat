40440680a0e25f88c125957cc4d5b73ba4100381
David Lougheed <david.lougheed@gmail.com>
2019-05-08 19:50:30 -0400

    Fix packaging.

---
+++ README_short.md
@@ -0,0 +1,8 @@
+# PyTrackDat — A pipeline for online data collection and management
+
+PyTrackDat comprises two Python scripts that analyze and assist in converting
+data and relevant metadata from `.csv` files into an online database that can
+facilitate data management, manipulation, and quality control. What each of
+these scripts does is outlined in this `README.md` file. Note that the care
+that is taken to assemble `.csv` files before using these scripts will help
+ensure that the database generated is maximally useful.
--- setup.py
+++ setup.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("README_short.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='PyTrackDat',
-    version='0.2.0',
+    name="pytrackdat",
+    version="0.2.0",
 
-    python_requires='~=3.6',
+    python_requires="~=3.6",
+    install_requires=["wheel", "virtualenv"],
 
     description='A utility for assisting in the creation of online '
                 'databases for biological data.',
-    url='https://github.com/ColauttiLab/PyTrackDat',
-    license='GPLv3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 
-    author='David Lougheed',
-    author_email='david.lougheed@gmail.com',
+    url="https://github.com/ColauttiLab/PyTrackDat",
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"  # Maybe should be refined
+    ],
 
-    packages=find_packages(),
+    author="David Lougheed",
+    author_email="david.lougheed@gmail.com",
+
+    packages=["pytrackdat"],
     include_package_data=True,
 
     entry_points={
-        'console_scripts': ['ptd-analyze=pytrackdat.analysis:main',
-                            'ptd-generate=pytrackdat.generation:main']
+        "console_scripts": ["ptd-analyze=pytrackdat.analysis:main",
+                            "ptd-generate=pytrackdat.generation:main"]
     }
 )
