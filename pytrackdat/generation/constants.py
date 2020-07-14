d6264832458f37461c7294401a4696d15e4b8b39
David Lougheed <david.lougheed@gmail.com>
2020-07-13 20:31:17 -0400

    Add pagination and filtering to API

--- pytrackdat/generation/constants.py
+++ pytrackdat/generation/constants.py
@@ -263,7 +263,12 @@ STATIC_NEW = """STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')"""
 
 REST_FRAMEWORK_SETTINGS = """
-REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']}
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,  # Default
+}
 """
 
 SPATIALITE_SETTINGS = """
