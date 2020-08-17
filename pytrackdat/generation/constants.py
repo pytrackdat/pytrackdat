ae8e80b1e523a46e4af752661854d4de3a40b5d9
David Lougheed <david.lougheed@gmail.com>
2020-08-16 20:16:35 -0400

    Add ordering filter to DRF

--- pytrackdat/generation/constants.py
+++ pytrackdat/generation/constants.py
@@ -266,7 +266,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')"""
 
 REST_FRAMEWORK_SETTINGS = """
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ],
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,  # Default
