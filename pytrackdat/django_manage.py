de837a568af3e19f1b7fe1f9a434e0192e7a6c75
David Lougheed <david.lougheed@gmail.com>
2021-02-18 21:08:46 -0500

    Remove temp directory creation

--- pytrackdat/django_manage.py
+++ pytrackdat/django_manage.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
-import os
 from sys import argv
 
 from pytrackdat.common import print_license
 from pytrackdat.ptd_site.manage import main as manage_main
-
-
-TEMP_DIRECTORY = os.path.join(os.getcwd(), "tmp")
 
 
 def main():
