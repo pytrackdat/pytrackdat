ba70aabf6e0eb9c78252d947a0fa72bdac30eac4
David Lougheed <david.lougheed@gmail.com>
2021-04-25 15:44:39 -0400

    Fix design file-related tests

--- tests/test_design_file_parsing.py
+++ tests/test_design_file_parsing.py
@@ -1,5 +1,5 @@
 # PyTrackDat is a utility for assisting in online database creation.
-# Copyright (C) 2018-2019 the PyTrackDat authors.
+# Copyright (C) 2018-2021 the PyTrackDat authors.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,24 +19,24 @@
 
 import unittest
 
-from pytrackdat.generation import design_to_relations
-from pytrackdat.generation.errors import GenerationError
+from pytrackdat.design_file import design_to_relations
+from pytrackdat.design_file.errors import DesignFileError
 
 
 class TestDesignFileParsing(unittest.TestCase):
     def test_two_primary_keys(self):
-        with self.assertRaises(GenerationError):
+        with self.assertRaises(DesignFileError):
             with open("./tests/design_files/two_primary_keys_1.csv") as tf:
                 design_to_relations(tf, False)
-        with self.assertRaises(GenerationError):
+        with self.assertRaises(DesignFileError):
             with open("./tests/design_files/two_primary_keys_2.csv") as tf:
                 design_to_relations(tf, False)
-        with self.assertRaises(GenerationError):
+        with self.assertRaises(DesignFileError):
             with open("./tests/design_files/two_primary_keys_3.csv") as tf:
                 design_to_relations(tf, False)
 
     def test_invalid_data_types(self):
-        with self.assertRaises(GenerationError):
+        with self.assertRaises(DesignFileError):
             with open("./tests/design_files/invalid_data_type.csv") as tf:
                 design_to_relations(tf, False)
 
