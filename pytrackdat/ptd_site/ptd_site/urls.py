15c90a076fce4336f75c6c795ab280ec3f83f1ea
David Lougheed <david.lougheed@gmail.com>
2021-05-16 23:39:24 -0400

    fix: url priority for ptd_site

--- pytrackdat/ptd_site/ptd_site/urls.py
+++ pytrackdat/ptd_site/ptd_site/urls.py
@@ -7,9 +7,9 @@ from pytrackdat.ptd_site.snapshot_manager.views import urls as snapshot_urls
 
 urlpatterns = [
     *snapshot_urls,
-    path("", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include(api_router.urls)),
     path("advanced_filters/", include("advanced_filters.urls")),
+    path("", admin.site.urls),
 ]
